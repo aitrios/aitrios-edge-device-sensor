5f5a77b394d8035760f173143cb10f34fce12dda
Ryota Kusakabe <Ryota.Kusakabe@sony.com>
2025-09-16 16:30:03 +0900

    Commit source code corresponding to version v0.1.11
    Signed-off-by: Ryota Kusakabe <Ryota.Kusakabe@sony.com>

---
+++ src/esf/sensor_main/src/raspberry_pi/sensor_main_impl.c
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2025 Sony Semiconductor Solutions Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "sensor_main_impl.h"
+
+EsfSensorErrCode EsfSensorUtilityResetFilesImpl(void) {
+  return kEsfSensorOk;
+}
--- src/esf/sensor_main/src/sensor_main.c
+++ src/esf/sensor_main/src/sensor_main.c
@@ -6,12 +6,97 @@
 
 #include "sensor_main.h"
 
+#include "senscord/c_api/senscord_c_api.h"
+#include "utility_log.h"
+#include "utility_log_module_id.h"
+#include "sensor_main_impl.h"
+
+#define ESF_SENSOR_MAIN_LOG(level, fmt, ...)                                  \
+  UtilityLogWriteDLog(MODULE_ID_SENSOR, level, "[%s:%d] " fmt "\n", __FILE__, \
+                      __LINE__, ##__VA_ARGS__)
+
+#define LOGE(fmt, ...) \
+  ESF_SENSOR_MAIN_LOG(kUtilityLogDlogLevelError, fmt, ##__VA_ARGS__)
+
+#define SENSCORD_PRINT_ERROR(target)                                     \
+  do {                                                                   \
+    char msg[128]   = {0};                                               \
+    uint32_t length = sizeof(msg);                                       \
+    senscord_get_last_error_string(SENSCORD_STATUS_PARAM_MESSAGE, msg,   \
+                                   &length);                             \
+    LOGE("%s err=%d: %s", target, senscord_get_last_error_cause(), msg); \
+  } while (0)
+
+extern int senscord_init_native_lib(void);
+extern void senscord_deinit_native_lib(void);
+
+static senscord_core_t core_ = 0;
+static int native_initialized_ = 0;
+
 EsfSensorErrCode EsfSensorInit(void) {
+  int ret;
+  senscord_config_t config;
+
+  if (core_ != 0) {
+    LOGE("senscord core has already been initialized");
+    goto error;
+  }
+
+  ret = senscord_config_create(&config);
+  if (ret != 0) {
+    SENSCORD_PRINT_ERROR("senscord_config_create");
+    goto error;
+  }
+
+  ret = senscord_core_init_with_config(&core_, config);
+  if (ret != 0) {
+    SENSCORD_PRINT_ERROR("senscord_core_init_with_config");
+    senscord_config_destroy(config);
+    goto error;
+  }
+
+  senscord_config_destroy(config);
+
+  ret = senscord_init_native_lib();
+  if (ret != 0) {
+    SENSCORD_PRINT_ERROR("senscord_init_native_lib");
+    goto error;
+  }
+  native_initialized_ = 1;
+
   return kEsfSensorOk;
+
+error:
+  if (core_ != 0) {
+    ret = senscord_core_exit(core_);
+    if (ret != 0) {
+      SENSCORD_PRINT_ERROR("senscord_core_exit");
+    }
+    core_ = 0;
+  }
+
+  return kEsfSensorFail;
 }
 
 EsfSensorErrCode EsfSensorExit(void) {
+  if (native_initialized_ != 0) {
+    senscord_deinit_native_lib();
+    native_initialized_ = 0;
+  }
+
+  if (core_ != 0) {
+    int ret = senscord_core_exit(core_);
+    if (ret != 0) {
+      SENSCORD_PRINT_ERROR("senscord_core_exit");
+      goto error;
+    }
+    core_ = 0;
+  }
+
   return kEsfSensorOk;
+
+error:
+  return kEsfSensorFail;
 }
 
 void EsfSensorPowerOFF(void) {
@@ -26,94 +111,12 @@ EsfSensorErrCode EsfSensorUtilityVerifyFiles(void) {
   return kEsfSensorOk;
 }
 
-#if defined(CONFIG_SENSOR_TARGET_T4)
-#define IMX500_DNN_FILE_DIR "/misc/imx500/ai_models/"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
-#include <sys/stat.h>
-#include <unistd.h>
-
-// PATH_MAX should be defined in limits.h, but provide fallback for environments
-// where it's not
-#ifndef PATH_MAX
-#define PATH_MAX 4096
-#endif
-
-static int RemoveDirRecursive(const char *path) {
-  if (path == NULL) {
-    return -1;
-  }
-  DIR *d = opendir(path);
-  int r = -1;
-  if (d) {
-    struct dirent *p;
-    r = 0;
-    while (!r && (p = readdir(d))) {
-      if (!strcmp(p->d_name, ".") || !strcmp(p->d_name, ".."))
-        continue;
-      char buf[PATH_MAX];
-      snprintf(buf, sizeof(buf), "%s/%s", path, p->d_name);
-      struct stat statbuf;
-      if (!stat(buf, &statbuf)) {
-        if (S_ISDIR(statbuf.st_mode)) {
-          r = RemoveDirRecursive(buf);
-        } else {
-          r = remove(buf);
-        }
-      }
-    }
-    closedir(d);
-  }
-  if (!r) {
-    r = rmdir(path);
-  }
-  return r;
-}
-
-static int RemoveDirRecursiveExceptPreinstall(const char *path) {
-  if (path == NULL) {
-    return -1;
-  }
-  DIR *d = opendir(path);
-  int r = -1;
-  if (d) {
-    struct dirent *p;
-    r = 0;
-    while (!r && (p = readdir(d))) {
-      if (!strcmp(p->d_name, ".") || !strcmp(p->d_name, ".."))
-        continue;
-      char buf[PATH_MAX];
-      snprintf(buf, sizeof(buf), "%s/%s", path, p->d_name);
-      struct stat statbuf;
-      if (!stat(buf, &statbuf)) {
-        if (S_ISDIR(statbuf.st_mode)) {
-          // Keep directories with "_preinstall" in their name
-          if (strstr(p->d_name, "_preinstall") != NULL) {
-            continue;
-          }
-          r = RemoveDirRecursive(buf);
-        } else {
-          r = remove(buf);
-        }
-      }
-    }
-    closedir(d);
-  }
-  return r;
-}
-#endif  // defined(CONFIG_SENSOR_TARGET_T4)
 
 EsfSensorErrCode EsfSensorUtilityResetFiles(void) {
-#if defined(CONFIG_SENSOR_TARGET_T4)
-  // Remove everything under IMX500_DNN_FILE_DIR except
-  // directories with "_preinstall" in their name
-  int ret = RemoveDirRecursiveExceptPreinstall(IMX500_DNN_FILE_DIR);
-  if (ret != 0) {
-    return kEsfSensorFail;
+  EsfSensorErrCode ret = EsfSensorUtilityResetFilesImpl();
+  if (ret != kEsfSensorOk) {
+    SENSCORD_PRINT_ERROR("EsfSensorUtilityResetFilesImpl failed");
+    return ret;
   }
-#endif  // defined(CONFIG_SENSOR_TARGET_T4)
   return kEsfSensorOk;
 }
