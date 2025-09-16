302e05095c972d29e85a5ac931c51b708c445913
Ryota Kusakabe <131426787+ryotakusakabe@users.noreply.github.com>
2025-09-16 20:50:23 +0900

    Commit source code corresponding to version v0.1.11 (#7)
    Signed-off-by: Ryota Kusakabe <Ryota.Kusakabe@sony.com>

---
+++ src/esf/sensor_main/src/sensor_main_impl.h
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2025 Sony Semiconductor Solutions Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef EDC_SENSOR_MAIN_IMPL_H_
+#define EDC_SENSOR_MAIN_IMPL_H_
+#include "sensor_main.h"
+
+EsfSensorErrCode EsfSensorUtilityResetFilesImpl(void);
+#endif /* EDC_SENSOR_MAIN_IMPL_H_ */
